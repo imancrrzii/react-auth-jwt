9cfedbb65a12b45749e6bf62971ada315bb47866
iman <imancarrazi777@gmail.com>
2025-08-19 09:40:19 +0700

    feat: add feature component for sifina games

--- src/components/games/Feature.jsx
+++ src/components/games/Feature.jsx
@@ -4,32 +4,109 @@ import { IoMdCheckmark } from "react-icons/io";
 import { Link } from "react-router-dom";
 import gambar5 from "../../assets/images/games/7.png";
 import gambar6 from "../../assets/images/games/5.png";
+import {
+  ArrowRightCircleIcon,
+  ArrowUpRight,
+  BookCopy,
+  LandPlot,
+  MonitorCheck,
+  MoveUpRight,
+  Ribbon,
+  Ship,
+} from "lucide-react";
 
 const Feature = () => {
   return (
-    <div className="bg-white relative min-h-[600px] py-12">
+    <div className="w-full bg-white relative min-h-[600px] p-12">
       <div className="flex flex-col items-center overflow-visible relative">
         <img
           src={gambar5}
           alt=""
-          className="absolute -top-16 right-36 w-32 h-32 max-md:hidden"
+          className="absolute -top-16 right-28 w-32 h-32 max-lg:hidden"
         />
         <img
           src={gambar6}
           alt=""
-          className="absolute -top-16 left-36 w-32 h-32 max-md:hidden"
+          className="absolute -top-16 left-28 w-32 h-32 max-lg:hidden"
         />
-        <div className="flex flex-col w-full justify-center items-center">
+        <div className="w-full max-w-screen-xl mx-auto px-6 md:px-12 flex flex-col gap-8 items-center">
+          {/* Heading */}
           <div className="text-center">
-            <h3
-              className="text-xl lg:text-3xl font-bold max-w-lg text-center lg:leading-tight text-gray-900 drop-shadow-lg"
-              style={{
-                textShadow:
-                  "2px 2px 4px rgba(255,255,255,0.8), -1px -1px 2px rgba(255,255,255,0.8)",
-              }}
-            >
+            <h3 className="text-xl lg:text-3xl font-bold max-w-lg ...">
               Why Traditional Hiring Processes Are Holding You Back
             </h3>
+          </div>
+
+          {/* Fitur cards */}
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 justify-between items-center mt-6 w-full">
+            <div className="flex flex-col space-y-4 bg-purple-100 p-6 items-start rounded-xl min-w-[210px]">
+              <BookCopy className="w-7 h-7" />
+              <h3 className="text-gray-800 font-bold">
+                Resume
+                <br />
+                Screening
+              </h3>
+            </div>
+            <div className="flex-flex-col space-y-4 bg-pink-100 p-6 items-start rounded-xl min-w-[210px]">
+              <div>
+                <LandPlot className="w-7 h-7" />
+              </div>
+              <h3 className="text-gray-800 font-bold">
+                Candidate <br /> Personality
+              </h3>
+            </div>
+            <div className="flex-flex-col space-y-4 bg-blue-100 p-6 items-start rounded-xl min-w-[210px]">
+              <div>
+                <MonitorCheck className="w-7 h-7" />
+              </div>
+              <h3 className="text-gray-800 font-bold">
+                AI sentiment <br /> Analysis
+              </h3>
+            </div>
+            <div className="flex-flex-col space-y-4 bg-orange-100 p-6 items-start rounded-xl min-w-[210px]">
+              <div>
+                <Ribbon className="w-7 h-7" />
+              </div>
+              <h3 className="text-gray-800 font-bold">
+                Hiring AI <br /> Reports
+              </h3>
+            </div>
+            <div className="flex-flex-col space-y-4 bg-green-100 p-6 items-start rounded-xl min-w-[210px]">
+              <div>
+                <Ship className="w-7 h-7" />
+              </div>
+              <h3 className="text-gray-800 font-bold">
+                AI Skill <br /> Assessments
+              </h3>
+            </div>
+          </div>
+
+          <div className="w-full bg-blue-500 rounded-xl">
+            <div className="px-6 md:px-8 py-6 flex flex-col md:flex-row justify-between text-center items-center">
+              <div className="text-white font-normal text-start">
+                <h3 className="max-w-xs">
+                  AI-powered resume screening and shortlisting
+                </h3>
+              </div>
+              <div className="flex gap-4 justify-center mt-4 md:mt-0 items-center">
+                <button className="bg-white text-sky-600 rounded-full py-3 pl-3 pr-2 font-semibold flex items-center gap-2">
+                  <p className="text-gray-800 font-medium text-sm me-1">
+                    Let's Collaborate
+                  </p>
+                  <div className="p-1 bg-blue-500 rounded-full">
+                    <ArrowUpRight className="w-3 h-3 text-white" />
+                  </div>
+                </button>
+                <button className="bg-transparent border border-white text-white rounded-full py-3 pl-3 pr-2 font-semibold flex items-center gap-2">
+                  <p className="text-white font-medium text-sm me-1">
+                    View Details
+                  </p>
+                  <div className="p-1 bg-white rounded-full">
+                    <ArrowUpRight className="w-3 h-3 text-black" />
+                  </div>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
